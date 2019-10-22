e899e7b20360ee6f683975fab89f8ce7698364ce
Daniel Zlotin <danielz@fb.com>
2019-10-23 01:29:19 +0300

    fix crash on sandbox mode

--- src/control.lua
+++ src/control.lua
@@ -1,9 +1,9 @@
 
 script.on_event(defines.events.on_player_created, function (event)
-  local player = game.players[event.player_index]
-  if player then
-    player.character_build_distance_bonus=60
-    player.character_reach_distance_bonus=60
-    player.character_running_speed_modifier=1
-  end
+	local player = game.players[event.player_index]
+	if player and player.character then
+		player.character_build_distance_bonus=60
+		player.character_reach_distance_bonus=60
+		player.character_running_speed_modifier=1
+	end
 end)
