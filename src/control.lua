1ca92017bf2c13460cf27121238e6372cb543ed2
Daniel Zlotin <danielz@fb.com>
2020-01-22 19:30:21 +0200

    long reach and 0.18

--- src/control.lua
+++ src/control.lua
@@ -2,8 +2,8 @@
 script.on_event(defines.events.on_player_created, function (event)
 	local player = game.players[event.player_index]
 	if player and player.character then
-		player.character_build_distance_bonus=60
-		player.character_reach_distance_bonus=60
+		player.character_build_distance_bonus=100
+		player.character_reach_distance_bonus=100
 		player.character_running_speed_modifier=1
 	end
 end)
