a4c2d3df553db1c91f86f942a8867cd36b400dbd
DanielZlotin <6660239+DanielZlotin@users.noreply.github.com>
2021-11-12 00:14:15 +0200

    fix broken pollution color api

--- src/data.lua
+++ src/data.lua
@@ -1,4 +1,4 @@
 path = "__LaziestBastard__"
 require "prototypes.crash-site-machines"
 require "prototypes.crash-site-items"
-data.raw["utility-sprites"]["default"].pollution_visualization.filename = path .. "/graphics/white-pollution-visualization.png"
+data.raw["utility-constants"]["default"].chart.pollution_color = {r = 140, g = 140, b = 140, a = 149}
